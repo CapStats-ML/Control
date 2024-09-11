cf0c0fe721843707910def90f9685224fb08ff03
Cesar Augusto Prieto Sarmiento <ceprieto@unal.edu.co>
2024-09-11 17:43:15 -0500

    CEC_Entrega2 Corregida
    - Se completo la entrega 2
    - Sugerencia, carta U para punto 4a

--- Entregas/Entrega 2/CEC_Entrega2.R
+++ Entregas/Entrega 2/CEC_Entrega2.R
@@ -75,52 +75,134 @@ legend(x = 25, y = -1.5 , legend = c('UCL & LCL', 'Obs', 'LC'), title = 'Lineas'
        lwd = c(1.8, 1.8, 1.8), cex = 0.8, bty = 'n', bg = 'white', text.font = 2,
        text.col = '#423f32')
 
-# Evaluemos el rendimento de la carta de control, para ellos vamos a calcular el ARL
-
-# Función para calcular la probabilidad de estar dentro de los límites de control
-Prob_in_control <- function(p0, n){
-  # Cálculo de Z utilizando la proporción nominal p0
-  LCL <- -3  # Límite inferior
-  UCL <- 3   # Límite superior
-  
-  # La probabilidad de que Z esté dentro de los límites sigue una distribución normal estándar
-  P_in_control <- pnorm(UCL, mean = 0, sd = 1) - pnorm(LCL, mean = 0, sd = 1)
-  
-  return(P_in_control)
-}
-
-# Calcular el ARL bajo control
-P_in_control <- Prob_in_control(p0, tamaños)
-ARL_bajo_control <- 1 / (1 - P_in_control)
-
-# Mostrar el ARL
-ARL_bajo_control
-
 # Comprobemos 4 con corrimientos de 0.025 hacia arriba y hacia abajo
 
 P0_i <- seq(0.025, 0.2, 0.025)
 
-# Aplica la funcion StanCC para cada valor de P0_i y tamaños
+# Generamos una muestra para cada P0_i 
 
-Matriz_Zi <- sapply(P0_i, function(p0_i) StanCC(tamaños, proporciones, p0_i))
+Muestra <- matrix(0, ncol = length(P0_i), nrow = m)
 
-# Calcular el ARL para cada valor de P0_i 
+for (i in 1:length(P0_i)){
+  Muestra[, i] <- rbinom(n = m, size = tamaños, prob = P0_i[i])
+  Muestra[, i] <- Muestra[, i] / tamaños
+}
 
-ARL_i <- sapply(P0_i, function(p0_i) 1 / (1 - Prob_in_control(p0_i, tamaños)))
+# Generamos la Matriz de Muestras estandarisadas dependiendo solo teniendo en cuenta p0
 
-colMeans(Matriz_Zi)
-var(Matriz_Zi)
+Zi_values <- matrix(0, ncol = length(P0_i), nrow = m)
+
+for (i in 1:length(P0_i)){
+  Zi_values[, i] <- StanCC(tamaños, Muestra[, i], p0)
+}
+
+# Graficando las cartas de control 
+
+par(mfrow = c(2, 2), mar = c(5.1, 5.1, 4.1, 2.1), bg = '#fffbf7') # Configurar márgenes y fondo
+
+for (i in 1:length(P0_i)){
+  plot(Zi_values[, i], type = 'o', col = '#423f32', pch = 20, cex = 1.5, ylim = c( min(Zi_values[, i]- 3, -3) , max(Zi_values[, i] + 1, 3)), family = "sans", bty = "L",
+       xlab = 'Tiempo de medición', ylab = 'Valor de Zi para \n Prop. No Conformidad',  font.lab = 2, cex.lab = 0.8,
+       font = 3, font.main = 4, main = paste('Carta de control Z con límites variables para p =', P0_i[i]), cex.axis = 0.7, col.main = 'black',
+       sub = paste('Proporción de no conformidad p =', P0_i[i]), font.sub = 2, cex.sub = 0.8, fg = '#423f32', col.sub = '#ff6f61')
+  
+  # Líneas de guía
+  abline(h = seq(-3, 3, 0.5), col = '#d6c6b8', lty = 'dotted')
+  abline(v = seq(1, m, 5), col = '#d6c6b8', lty = 'dotted')
+  
+  # Líneas de control superior e inferior
+  abline(h = UCL, col = '#ff6f61', lty = 'dashed', lwd = 1.8)
+  abline(h = LCL, col = '#ff6f61', lty = 'dashed', lwd = 1.8)
+  
+  # Línea central (valor nominal de Z = 0)
+  abline(h = 0, col = '#6b8e8e', lwd = 1.2)
+}
+
+RunLenghtpVar = function(n = seq(200,400,50), p = 0.1, SimularPorVez = 20000, 
+                         L = 3, m = 50000, corrimiento = 0.1){
+  UCL = L        # Límites superiores
+  LCL = -L        # Límites inferiores
+  RL = c()
+  pb = txtProgressBar(min = 0, max = m, style = 3)
+  longitud = 1
+  while(length(RL) < m){
+    tamaños = sample(x = n, size = SimularPorVez,   # Se generan 100 muestras de los 
+                     replace = T)                   # posibles tamaños sin reemplazo
+    muestras = rbinom(n = SimularPorVez, size = tamaños, 
+                      prob = corrimiento)
+    proporcion = muestras/tamaños
+    proporciones = StanCC(tamaños, proporcion, p)
+    for (i in 1:SimularPorVez){
+      if (proporciones[i] < UCL & proporciones[i] > LCL){
+        longitud = longitud + 1
+      } else {
+        RL = c(RL, longitud)
+        longitud = 1
+        setTxtProgressBar(pb, value = length(RL))
+      }
+    }
+  }
+  return(RL)
+}
+
+corrimientos = seq(0.025, 0.2, by = 0.025)
+ARL = numeric(length(corrimientos))
+
+for (i in 1:length(ARL)){
+  ARL[i] = mean(RunLenghtpVar(corrimiento = corrimientos[i]))
+}
 
 
+# Para obtener un ARL de 374 toca mirar p = 0.5 con corrimientos al rededor de este 
 
+StanCC <- function(n, P, p0){
+  Zi <- (P - p0) / sqrt(p0 * (1 - p0) / n)
+  return(Zi)
+}
 
+RunLenghtpVar = function(n = seq(200,400,50), p = 0.5, SimularPorVez = 25000, 
+                         L = 3, m = 50000, corrimiento = 0.1){
+  # Límites de control estandarizados
+  UCL = L
+  LCL = -L
+  RL = c()
+  pb = txtProgressBar(min = 0, max = m, style = 3)
+  longitud = 1
+  
+  while(length(RL) < m){
+    # Generación de tamaños de muestra y proporciones
+    tamaños = sample(x = n, size = SimularPorVez, replace = TRUE)
+    muestras = rbinom(n = SimularPorVez, size = tamaños, prob = corrimiento)
+    proporcion = muestras / tamaños
+    
+    # Estandarización
+    proporciones = StanCC(tamaños, proporcion, p)
+    
+    # Evaluación de las proporciones estandarizadas con respecto a los límites
+    for (i in 1:SimularPorVez){
+      if (proporciones[i] < UCL & proporciones[i] > LCL){
+        longitud = longitud + 1
+      } else {
+        RL = c(RL, longitud)
+        longitud = 1
+        setTxtProgressBar(pb, value = length(RL))
+      }
+    }
+  }
+  return(RL)
+}
 
+# Simulación con diferentes corrimientos
+corrimientos = seq(0.4, 0.6, by = 0.025)
+ARL = numeric(length(corrimientos))
 
+set.seed(123)
+for (i in 1:length(corrimientos)){
+  RL = RunLenghtpVar(corrimiento = corrimientos[i])  # Simula el Run Length
+  ARL[i] = mean(RL)  # Calcula el ARL como el promedio de los RL simulados
+}
 
-
-
-
-
+print(ARL)
 
 
 
@@ -142,12 +224,11 @@ var(Matriz_Zi)
 
 # Importacion de los datos
 
-Periodo1 <- read.csv("~/REPOS GIT/Control/Trabajos/Entrega 2/Periodo1.txt", 
+Periodo1 <- read.csv("~/REPOS GIT/Control/Entregas/Entrega 2/Periodo1.txt", 
                      colClasses = c("character", "numeric", "numeric"), 
                      col.names = c("Mes", "Dias", "DaysBetween"))
 
 # Quitar NA's 
-
 drop_na <- function(df){
   df <- df[complete.cases(df), ]
   return(df)
@@ -156,7 +237,133 @@ drop_na <- function(df){
 sum(is.na(Periodo1$Dias))
 sum(is.na(Periodo1$DaysBetween))
 
-Periodo1 <- drop_na(Periodo1)
+
+### Carta C para el numero de homicidios por mes ----
+
+NH <- numeric(12)
+
+# Agrupar y contar por mes el número de homicidios
+
+names <- c("Jan", "Feb", "March", "April", "May", "June",
+           "July", "Aug", "Sep", "Oct", "Nov", "Dec")
+
+for (i in 1:12){
+  NH[i] <- sum(Periodo1$Mes == names[i])
+}
+
+C_barra <- sum(NH)/length(NH)
+
+UCL <- C_barra + 3*sqrt(C_barra)
+CL <- C_barra
+LCL <- C_barra - 3*sqrt(C_barra)
+
+# Gráfico de la carta C
+
+par(mfrow = c(1,1), mar = c(5.1, 5.1, 4.1, 2.1), bg = '#fffbf7') # Configurar márgenes y fondo
+
+plot(NH, type = 'o', col = '#423f32', pch = 20, cex = 1.5, ylim = c(0, UCL + 1), family = "sans", bty = "L",
+     xlab = 'Mes', ylab = 'Número de homicidios',  font.lab = 2, cex.lab = 0.8,
+     font = 3, font.main = 4, main = 'Carta de control C', cex.axis = 0.7, col.main = 'black',
+     sub = 'Número de homicidios por mes', font.sub = 2, cex.sub = 0.8, fg = '#423f32', col.sub = '#ff6f61')
+
+# Líneas de guía
+
+abline(h = seq(0, UCL, 1), col = '#d6c6b8', lty = 'dotted')
+abline(v = seq(1, 12, 1), col = '#d6c6b8', lty = 'dotted')
+
+# Líneas de control superior e inferior
+
+abline(h = UCL, col = '#ff6f61', lty = 'dashed', lwd = 1.8)
+abline(h = CL, col = '#6b8e8e', lty = 'dashed', lwd = 1.8)
+abline(h = 0, col = '#ff6f61', lty = 'dashed', lwd = 1.8)
+
+# Leyenda 
+
+legend(x = 9, y = 7 , legend = c('UCL & LCL', 'Obs', 'LC'), title = 'Lineas',
+       col = c('#ff6f61', '#423f32', '#6b8e8e'), lty = c('dashed', 'solid', 'solid'), 
+       lwd = c(1.8, 1.8, 1.8), cex = 0.7, bty = 'n', bg = 'white', text.font = 2,
+       text.col = '#423f32')
+
+# Carta con limites exponenciales  --------
+
+#Con estos datos podemos hacer una carta con límites exponenciales y
+#una carta con límites poisson agrupando por meses
+
+
+#Carta con límites de probabilidad exponenciales
+
+#Empezamos por estimar p
+p_hat <- 1/(mean(as.numeric(Periodo1[,3]), na.rm = TRUE))
+
+#De momento podemos tomar que los fechas siguen una distribución exp
+#de parametro p_hat, seguímos por establecer límites de control.
+
+
+#Límite inferior: la cola inferior de la dist. exp. que acomula 0.0027/2
+
+LCL_1 <- qexp(0.00135, rate = p_hat)
+
+#Límite superior: la cola superior que acumula el 0.0027/2
+
+UCL_1 <- qexp(0.00135, rate = p_hat, lower.tail = FALSE)
+
+CL <- qexp(0.5, rate = p_hat)
+
+franja_m2 <- qexp(0.022, rate = p_hat)
+franja_m1 <- qexp(0.15, rate = p_hat)
+
+franja_p1 <- qexp(0.84, rate = p_hat)
+franja_p2 <- qexp(0.97, rate = p_hat)
+
+
+
+# Ahora para graficar la carta 
+
+par(mar = c(5.1, 5.1, 4.1, 2.1), bg = '#fffbf7') # Configurar márgenes y fondo
+
+plot(Periodo1$DaysBetween, type = 'o', col = '#423f32', pch = 20, cex = 1.5, ylim = c(0, UCL_1 + 1), family = "sans", bty = "L",
+     xlab = 'Observación', ylab = 'Días entre homicidios',  font.lab = 2, cex.lab = 0.8,
+     font = 3, font.main = 4, main = 'Carta de control con límites exponenciales', cex.axis = 0.7, col.main = 'black',
+     sub = 'Días entre homicidios', font.sub = 2, cex.sub = 0.8, fg = '#423f32', col.sub = '#ff6f61')
+
+# Líneas de guía
+
+abline(h = seq(0, UCL_1, 5), col = '#d6c6b8', lty = 'dotted')
+abline(v = seq(0, 30, 5), col = '#d6c6b8', lty = 'dotted')
+
+# Líneas de control superior e inferior
+
+abline(h = UCL_1, col = '#ff6f61', lty = 'dashed', lwd = 1.8)
+abline(h = franja_p2, col = '#FAC30E', lty = 'dashed', lwd = 1.8)
+abline(h = franja_p1, col = '#FAEB0E', lty = 'dashed', lwd = 1.8)
+
+abline(h = LCL_1, col = '#ff6f61', lty = 'dashed', lwd = 1.8)
+abline(h = franja_m2, col = '#FAC30E', lty = 'dashed', lwd = 1.8)
+abline(h = franja_m1, col = '#FAEB0E', lty = 'dashed', lwd = 1.8)
+
+# Línea central (valor nominal de Z = 0)
+
+abline(h = CL, col = '#6b8e8e', lwd = 1.8)
+
+# Leyenda
+
+legend(x = 20, y = 75 , legend = c('UCL & LCL', 'Obs', 'LC'), title = 'Lineas',
+       col = c('#ff6f61', '#423f32', '#6b8e8e'), lty = c('dashed', 'solid', 'solid'), 
+       lwd = c(1.8, 1.8, 1.8), cex = 0.7, bty = 'n', bg = 'white', text.font = 2,
+       text.col = '#423f32')
+
+
+# Problema: La dist. exp. no es simétrica, así que las reglas de sensibilidad
+# no funcionan de la misma forma, hallemos los equivalentes a +- 1 y 2 sigma
+
+ARL_1 <- 1/0.0027
+
+# Ahora veamoslo con una construccion con la Poisson
+
+
+# b) Transfórmense los datos usando la  x = y^(0.25) y diséñese una carta de control 
+#    apropiada para la situación.
+
 
 # Prueba de normalidad con qqplot
 
@@ -182,89 +389,7 @@ legend(x = 1, y = 5, legend = c('Tendencia', 'Observaciones'), col = c('#6b8e8e'
        bg = 'white', text.font = 2, text.col = '#423f32')
 
 
-# Calculos necesarios para construir la carta Rango Moviles y X 
-# Podemos entender la variables DaysBetween como el rango de días entre homicidios
-# Con lo cual no tenemos que calcular R para cada observacion sino R_barra y encontrar 
-# los respectivos valores de  D4 y D3 para los limites de control de la carta R
-
-# Calcular la media y la desviación estándar
-
-media <- mean(Periodo1$DaysBetween)
-sd <- sd(Periodo1$DaysBetween)
-
-# Límites de Control
-UCL <- media + 3 * sd
-LCL <- max(media - 3 * sd, 0)
-
-# Grafiar la carta de control
-
-par(mar = c(5.1, 5.1, 4.1, 2.1), bg = '#fffbf7') # Configurar márgenes y fondo
-
-plot(Periodo1$DaysBetween, type = 'o', col = '#423f32', pch = 20, cex = 1.5, ylim = c(LCL, UCL), family = "sans", bty = "L",
-     xlab = 'Observacion Temporal', ylab = 'Tiempo entre Ass \n Tranformadas',  font.lab = 2, cex.lab = 0.8,
-     font = 3, font.main = 4, main = 'Carta de control observaciones', cex.axis = 0.7, col.main = 'black',
-     sub = 'Tiempo entre asesinatos', font.sub = 2, cex.sub = 0.8, fg = '#423f32', col.sub = '#ff6f61')
-
-# Líneas de guía
-abline(h = seq(-30, 50, 5), col = '#d6c6b8', lty = 'dotted')
-abline(v = seq(0, 30, 2.5), col = '#d6c6b8', lty = 'dotted')
-
-# Líneas de control superior e inferior
-abline(h = UCL, col = '#ff6f61', lty = 'dashed', lwd = 1.8)
-abline(h = LCL, col = '#ff6f61', lty = 'dashed', lwd = 1.8)
-
-# Línea central (valor nominal de Z = 0)
-abline(h = media, col = '#6b8e8e', lwd = 1.2)
-
-# Leyenda
-legend(x = 20, y = 45 , legend = c('UCL & LCL', 'Obs', 'LC'), title = 'Lineas',
-       col = c('#ff6f61', '#423f32', '#6b8e8e'), lty = c('dashed', 'solid', 'solid'), 
-       lwd = c(1.8, 1.8, 1.8), cex = 0.8, bty = 'n', bg = 'white', text.font = 2,
-       text.col = '#423f32')
-
-# Carta de Rango Movil
-
-k <- 2
-rango_movil <- sapply(1:(length(Periodo1$DaysBetween) - k + 1), function(i) max(Periodo1$DaysBetween[i:(i + k - 1)]) - min(Periodo1$DaysBetween[i:(i + k - 1)]))
-
-# Media y desviación estándar de los rangos móviles
-media_rango <- mean(rango_movil)
-sd_rango <- sd(rango_movil)
-
-# Límites de Control para la carta de rango móvil
-UCL_rango <- media_rango + 3 * sd_rango
-LCL_rango <- max(media_rango - 3 * sd_rango, 0)
-
-# Graficar la carta de rango móvil
-
-par(mar = c(5.1, 5.1, 4.1, 2.1), bg = '#fffbf7') # Configurar márgenes y fondo
-
-plot(rango_movil, type = 'o', col = '#423f32', pch = 20, cex = 1.5, ylim = c(LCL_rango - 1, UCL_rango + 1), family = "sans", bty = "L",
-     xlab = 'Observacion Temporal', ylab = 'Rango del tiempo\nEntre Asesinatos',  font.lab = 2, cex.lab = 0.8,
-     font = 3, font.main = 4, main = 'Carta de control MR', cex.axis = 0.7, col.main = 'black',
-     sub = 'Tiempo entre asesinatos', font.sub = 2, cex.sub = 0.8, fg = '#423f32', col.sub = '#ff6f61')
-
-# Líneas de guía
-abline(h = seq(-20, 40, 5), col = '#d6c6b8', lty = 'dotted')
-abline(v = seq(0, 30, 2.5), col = '#d6c6b8', lty = 'dotted')
-
-# Líneas de control superior e inferior
-abline(h = UCL_rango, col = '#ff6f61', lty = 'dashed', lwd = 1.8)
-abline(h = LCL_rango, col = '#ff6f61', lty = 'dashed', lwd = 1.8)
-
-# Línea central (valor nominal de Z = 0)
-abline(h = media_rango, col = '#6b8e8e', lwd = 1.2)
-
-# Leyenda
-legend(x = 20, y = 35 , legend = c('UCL & LCL', 'Obs', 'LC'), title = 'Lineas',
-       col = c('#ff6f61', '#423f32', '#6b8e8e'), lty = c('dashed', 'solid', 'solid'), 
-       lwd = c(1.8, 1.8, 1.8), cex = 0.8, bty = 'n', bg = 'white', text.font = 2,
-       text.col = '#423f32')
-
-
-# b) Transfórmense los datos usando la  x = y^(0.25) y diséñese una carta de control 
-#    apropiada para la situación.
-
+# Transformar los datos ----
 
 Trans <- Periodo1$DaysBetween^(0.25)
 
@@ -291,8 +416,8 @@ legend(x = 1, y = 1.25, legend = c('Tendencia', 'Observaciones'), col = c('#6b8e
 
 # Calcular la media y la desviación estándar
 
-media <- mean(Trans)
-sd <- sd(Trans)
+media <- mean(Trans, na.rm = TRUE)
+sd <- sd(Trans, na.rm = TRUE)
 
 # Límites de Control
 UCL <- media + 3 * sd
@@ -330,8 +455,8 @@ k <- 2
 rango_movil <- sapply(1:(length(Trans) - k + 1), function(i) max(Trans[i:(i + k - 1)]) - min(Trans[i:(i + k - 1)]))
 
 # Media y desviación estándar de los rangos móviles
-media_rango <- mean(rango_movil)
-sd_rango <- sd(rango_movil)
+media_rango <- mean(rango_movil, na.rm = TRUE)
+sd_rango <- sd(rango_movil, na.rm = TRUE)
 
 # Límites de Control para la carta de rango móvil
 UCL_rango <- media_rango + 3 * sd_rango
